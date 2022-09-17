d904f8ae6be197a9752495337fdd5f2590921bfc
Júnia Camargo <juniatech@gmail.com>
2022-09-17 15:29:18 -0300

    Estrutura e comandos personalizados

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,54 @@
+// COMANDOS PERSONALIZADOS 
+
+// Deve impedir o cadastro do usuário com email já utilizado
+Cypress.Commands.add('POSTUsuarios400', () => { 
+    return cy.request({
+        method: 'POST',
+        url: '/usuarios', 
+        failOnStatusCode: false,
+        body: {
+            "nome": "Faye",
+            "email": "Laney20@gmail.com",
+            "password": "mzz3q3MSB6OHNzd",
+            "administrador": "true",
+        }
+    })
+ })
+
+// Deve listar todos os usuários cadastrados
+Cypress.Commands.add('rest', (method = 'GET', url = '/', body = null, failOnStatusCode = false) => {
+    return cy.request({
+        method: method,
+        url: url,
+        failOnStatusCode: failOnStatusCode, 
+        body: body
+    })
+})
+
+// Deve buscar um usuario para login 
+Cypress.Commands.add('buscarUsuarioParaLogin', () => {
+    cy.rest('GET', '/usuarios').then(resposta => {
+        expect(resposta.body).to.haveOwnProperty('usuarios')
+        return {
+            email: resposta.body.usuarios[0].email,
+            senha: resposta.body.usuarios[0].password 
+        }
+    })
+})
+
+// Deve realizar o login do usuario com sucesso 
+Cypress.Commands.add('logar', (email, senha) => {
+    return cy.request({
+        method: 'POST',
+        url: '/login',
+        failOnStatusCode: false,
+        body: {
+            "email": email,
+            "password": senha
+        }
+    })
+})
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
